886012d3a5cccfb6c1ba99d58021d5f7b4f908a8
kotsiuk dmitro <kotsiukdmitro@gmail.com>
2024-08-30 17:38:04 +0300

    add form

--- module3(form)/src/App.vue
+++ module3(form)/src/App.vue
@@ -1,47 +1,96 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="w-[800px] m-auto mt-10 border p-10 border-gray-600 border-r-2 rounded-xl">
+    <form @submit.prevent="submitHandler">
+      <h1 class="text-center font-semibold text-2xl mb-10">Анкета на VUE разработчика</h1>
+      <CustomInput label="Как тебя зовут?" placeholder="Введите имя" :error="errors.name" v-model="name" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="flex flex-col mb-6">
+        <label for="age" class="mb-2 italic">Выберите возраст</label>
+        <input v-model.number="age" type="number" id="age" max="50" min="18" class="border px-5 rounded-md" />
+      </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+      <div class="flex flex-col mb-6 italic">
+        <label for="city" class="mb-2">Ваш город</label>
+        <select id="city" class="border px-5 rounded-md" v-model="city">
+          <option value="zp">Запорожье</option>
+          <option value="kiev">Киев</option>
+          <option value="dp">Днепр</option>
+        </select>
+      </div>
+
+      <h2 lass="mb-2 italic ">Готов к переезду в другой город?</h2>
+      <label><input v-model="relocate" type="radio" name="trip" value="yes" /> Да</label> <br />
+      <label><input v-model="relocate" type="radio" name="trip" value="no" /> Нет</label>
+
+      <h2 class="mb-2 italic mt-3">Что знаешь o Vue?</h2>
+      <div>
+        <label><input v-model="skills" type="checkbox" name="skill" value="vuex" /> Vuex</label>
+      </div>
+      <div>
+        <label><input v-model="skills" type="checkbox" name="skill" value="cli"/> Vue CLI</label>
+      </div>
+      <div>
+        <label><input v-model="skills" type="checkbox" name="skill" value="router"/> Vue Router</label>
+      </div>
+
+      <div class="mt-5 flex">
+        <label class="ml-auto mr-10"><input v-model="agree" type="checkbox" /> Согласие на обработку данных</label>
+      </div>
+
+      <button type="submit" class="border p-2 text-white bg-slate-500 rounded-xl mt-5">
+        отправить
+      </button>
+    </form>
+  </div>
 </template>
 
+<script>
+import CustomInput from './components/CustomInput.vue';
+
+
+export default {
+  data() {
+    return {
+      name: '',
+      age: 23,
+      city: 'zp',
+      relocate: 'yes',
+      skills: [],
+      agree: null,
+      errors: {
+        name: null
+      }
+    }
+  },
+  methods: {
+    formIsValid() {
+      let isValid = true
+      if(this.name.length === 0) {
+        this.errors.name = 'Поле обязательно для заполнения'
+        isValid = false
+      }else {
+        this.errors.name = null
+      }
+      return isValid
+    },
+
+    submitHandler() {
+      if(this.formIsValid()) {
+      console.group('form data')
+      console.log('Name:', this.name);
+      console.log('Age:', this.age);
+      console.log('City:', this.city);
+      console.log('Готовность к переезду:', this.relocate);
+      console.log('Agree:', this.agree);
+      console.groupEnd()
+      }
+     
+    }
+  },
+  components: {CustomInput}
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ module3(form)/src/components/CustomInput.vue
@@ -0,0 +1,44 @@
+<template>
+ <div class="flex flex-col mb-6 relative"
+          >
+        <label :for="id" class="mb-2 italic">{{ label }}</label>
+        <input
+          type="text"
+          :id="id"
+          @value="modelValue"
+          @input="change"
+          :placeholder="placeholder"
+          class="border px-5 rounded-md"   
+          :class="{'border-red-600': error}"      
+        />
+        <small class="text-red-500 absolute -bottom-[20px]">{{ error }}</small>
+      </div>
+</template>
+
+<script>
+    export default {
+        emits:['update:modelValue'],
+        props: {
+            modelValue: String,
+            label: String,
+            placeholder: String,
+            error: String
+        },
+        data() {
+            return {
+                id: 'input-' + Math.random()
+            }
+        },
+        methods: {
+         change(event){
+            this.$emit('update:modelValue', event.target.value)
+
+         }   
+        },
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
