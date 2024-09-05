5b6ce05653e99969209d471b782b8fd60f6f236e
kotsiuk dmitro <kotsiukdmitro@gmail.com>
2024-09-05 10:18:42 +0300

    add project vuex

---
+++ module7-vuex/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import TheNavbar from './TheNavbar.vue'
+
+
+export default {
+  components: {TheNavbar}
+
+}
+
+</script>
+
+<template>
+  <TheNavbar />
+  <div class="h-screen bg-slate-400 pt-20">
+      <div class="bg-white p-5 m-auto w-[800px] rounded-xl">
+        <h1 class="text-center font-bold text-[24px] mb-10">Про VUEX</h1>
+        <h2 class="ml-10 font-semibold">Счётчик 0</h2>
+    </div>
+  </div>
+</template>
+
+
---
+++ module7-vuex/src/TheNavbar.vue
@@ -0,0 +1,11 @@
+<template>
+<header class="text-blue-400 text-[20px] flex px-10 py-2">
+    Vuex
+</header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
