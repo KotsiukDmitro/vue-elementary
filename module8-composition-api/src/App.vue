60f0a4ea32be26cb1c75f4cc0d0fbea285d6856e
kotsiuk dmitro <kotsiukdmitro@gmail.com>
2024-09-06 10:16:57 +0300

    add new project

---
+++ module8-composition-api/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+
+export default {
+
+data() {
+  return {
+    name: 'VueJS',
+    version: 3
+  }
+},
+  methods: {
+    changeInfo() {
+      this.name = 'Vue JS !!!'
+      this.version = 4
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="h-screen bg-slate-400 pt-20">
+    <div class="bg-white p-5 m-auto w-[800px] rounded-xl">
+      <h1 class="mb-2 text-2xl font-bold text-center">Vue Composition Api</h1>
+      <hr class="mb-10"/>
+      <p class="mb-5"> Название: <strong>{{ name }}</strong></p>
+      <p class="mb-10">Версия: <strong>{{ version }}</strong></p>
+
+      <button
+      @click="changeInfo"
+      class="border border-blue-600 text-white bg-blue-500 rounded-xl px-3 py-1 hover:bg-blue-400 mr-5 mb-10"
+    >
+      изменить
+    </button>
+  </div>
+  </div>
+</template>
+
+
